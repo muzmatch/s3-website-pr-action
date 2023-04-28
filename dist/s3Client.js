8be7bee0cb721884132d75628fc000828fd8a2b4
mjnoach <a.siennkiewicz@gmail.com>
2023-04-28 19:32:00 +0200

    remove dist dir

--- dist/s3Client.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_s3_1 = require("@aws-sdk/client-s3");
-exports.default = new client_s3_1.S3Client({ region: "us-east-1" });
