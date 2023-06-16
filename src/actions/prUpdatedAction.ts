5598f6c95240cd252d2c631705a5be1308d34a8a
mjnoach <a.siennkiewicz@gmail.com>
2023-06-16 19:50:27 +0200

    potibm:master Prevent AccessControlListNotSupported

--- src/actions/prUpdatedAction.ts
+++ src/actions/prUpdatedAction.ts
@@ -1,22 +1,24 @@
-import * as github from "@actions/github";
+import * as github from '@actions/github';
 import {
   CreateBucketCommand,
   PutBucketPolicyCommand,
   PutBucketWebsiteCommand,
   PutPublicAccessBlockCommand,
-} from "@aws-sdk/client-s3";
-import { ReposCreateDeploymentResponseData } from "@octokit/types";
-import githubClient from "../githubClient";
-import S3 from "../s3Client";
-import checkBucketExists from "../utils/checkBucketExists";
-import deactivateDeployments from "../utils/deactivateDeployments";
-import s3UploadDirectory from "../utils/s3UploadDirectory";
-import validateEnvVars from "../utils/validateEnvVars";
+  DeletePublicAccessBlockCommand,
+  DeleteBucketOwnershipControlsCommand,
+} from '@aws-sdk/client-s3';
+import { ReposCreateDeploymentResponseData } from '@octokit/types';
+import githubClient from '../githubClient';
+import S3 from '../s3Client';
+import checkBucketExists from '../utils/checkBucketExists';
+import deactivateDeployments from '../utils/deactivateDeployments';
+import s3UploadDirectory from '../utils/s3UploadDirectory';
+import validateEnvVars from '../utils/validateEnvVars';
 
 export const requiredEnvVars = [
-  "AWS_ACCESS_KEY_ID",
-  "AWS_SECRET_ACCESS_KEY",
-  "GITHUB_TOKEN",
+  'AWS_ACCESS_KEY_ID',
+  'AWS_SECRET_ACCESS_KEY',
+  'GITHUB_TOKEN',
 ];
 
 export default async (
@@ -28,17 +30,17 @@ export default async (
   const { repo } = github.context;
   const branchName = github.context.payload.pull_request!.head.ref;
 
-  console.log("PR Updated");
+  console.log('PR Updated');
 
   validateEnvVars(requiredEnvVars);
 
   const bucketExists = await checkBucketExists(bucketName);
 
   if (!bucketExists) {
-    console.log("S3 bucket does not exist. Creating...");
+    console.log('S3 bucket does not exist. Creating...');
     await S3.send(new CreateBucketCommand({ Bucket: bucketName }));
 
-    console.log("Update S3 bucket public policy...");
+    console.log('Update S3 bucket public policy...');
     await S3.send(
       new PutPublicAccessBlockCommand({
         Bucket: bucketName,
@@ -51,13 +53,13 @@ export default async (
       })
     );
 
-    console.log("Configuring bucket website...");
+    console.log('Configuring bucket website...');
     await S3.send(
       new PutBucketWebsiteCommand({
         Bucket: bucketName,
         WebsiteConfiguration: {
-          IndexDocument: { Suffix: "index.html" },
-          ErrorDocument: { Key: "404/index.html" },
+          IndexDocument: { Suffix: 'index.html' },
+          ErrorDocument: { Key: '404/index.html' },
         },
       })
     );
@@ -66,21 +68,39 @@ export default async (
       new PutBucketPolicyCommand({
         Bucket: bucketName,
         Policy: `{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${bucketName}/*"
-        }
-    ]
-}`,
+            "Version": "2012-10-17",
+            "Statement": [
+              {
+                "Sid": "",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::${bucketName}/*"
+              }
+            ]
+        }`,
       })
     );
+
+    console.log('Deleting public access block...');
+    await S3.send(
+      new DeletePublicAccessBlockCommand({
+        Bucket: bucketName,
+      })
+    );
+
+    try {
+      console.log('Delete Bucket ownership controls...');
+      await S3.send(
+        new DeleteBucketOwnershipControlsCommand({
+          Bucket: bucketName,
+        })
+      );
+    } catch (error) {
+      console.error('Error deleting bucket ownership controls:', error);
+    }
   } else {
-    console.log("S3 Bucket already exists. Skipping creation...");
+    console.log('S3 Bucket already exists. Skipping creation...');
   }
 
   await deactivateDeployments(repo, environmentPrefix);
@@ -88,7 +108,7 @@ export default async (
   const deployment = await githubClient.repos.createDeployment({
     ...repo,
     ref: `refs/heads/${branchName}`,
-    environment: `${environmentPrefix || "PR-"}${
+    environment: `${environmentPrefix || 'PR-'}${
       github.context.payload.pull_request!.number
     }`,
     auto_merge: false,
@@ -100,16 +120,16 @@ export default async (
     await githubClient.repos.createDeploymentStatus({
       ...repo,
       deployment_id: deployment.data.id,
-      state: "in_progress",
+      state: 'in_progress',
     });
 
-    console.log("Uploading files...");
+    console.log('Uploading files...');
     await s3UploadDirectory(bucketName, uploadDirectory);
 
     await githubClient.repos.createDeploymentStatus({
       ...repo,
       deployment_id: deployment.data.id,
-      state: "success",
+      state: 'success',
       environment_url: `${websiteUrl}/en-US/`,
     });
 
@@ -120,5 +140,5 @@ export default async (
 function isSuccessResponse(
   object: any
 ): object is ReposCreateDeploymentResponseData {
-  return "id" in object;
+  return 'id' in object;
 }
